421266e0540714a60ea943a68e6eb785f372d80b
c-abhijith <cabhijithofficial@gmail.com>
2024-10-30 22:30:17 +0530

    updated

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Load environment variables from .env file
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
